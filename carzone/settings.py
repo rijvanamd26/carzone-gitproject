7e8ca205e5fd5960dc32db200e1e1dfbff164e89
MD.Rijvana <rijvana.md26@gmail.com>
2022-08-30 21:15:12 +0530

    Completed Teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER':'postgres',
+        'PASSWORD':'post@888',
+        'HOST':'localhost',
     }
 }
 
@@ -112,14 +115,17 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+#USE_TZ = True(manaually did when makemigrations didnt work after registering postgresql in settings.py) 
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, "carzone/static"),
 ] 
+STATIC_ROOT=os.path.join(BASE_DIR, "static") 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
